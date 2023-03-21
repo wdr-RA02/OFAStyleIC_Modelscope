75b042b71e2b1674c01451dd74582b7af748fa45
wdr-RA02 <cydia1829@gmail.com>
2023-03-21 14:12:44 +0000

    impled gen_sample of scst criterion

--- model/scst_criterion/scst.py
+++ model/scst_criterion/scst.py
@@ -1,4 +1,5 @@
 import random
+
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -8,12 +9,26 @@ from torch.nn.modules.loss import _Loss
 
 
 class SelfCriticalSeqTrainingCriterion(_Loss):
-    def __init__(self, cfg:Config) -> None:
+    def __init__(self, args:Config) -> None:
+        self.cfg=args
+        self.tokenizer=args.tokenizer
+        # padding
+        self.padding_idx = args.tokenizer.pad_token_id
         super().__init__()
 
 
     def forward(self, model, inputs):
-        pass
+        assert isinstance(model, OfaForAllTasks), \
+            "model required to be OfaForAllTasks type, got {}".format(type(model))
+        
+        assert id(model.tokenizer)==id(self.tokenizer), \
+            "tokenizers from model and trainer don't match, this is usually unexpected. "
+        '''
+        gen_tgt_tokens: decode后的sampled_sequence [Tensor(n_words) * batch_size]
+        gen_tgt_words: sampled_sequence的token  [str * batch_size]
+        gt_words:  decode后的ground truth    [str * batch_size]
+        '''
+        gen_tgt_tokens, gen_tgt_words, gt_words=self.get_sample_from_beams(model, inputs)
 
     def get_sample_from_beams(self, model, inputs):
         '''
@@ -24,31 +39,37 @@ class SelfCriticalSeqTrainingCriterion(_Loss):
         inputs: train preprocessor的输出
         
         return:
-        gen_target: decode后的sampled_sequence
-        gen_res: sampled_sequence的token
+        gen_target: sampled_sequence的token list
+        gen_res: decode后的sampled_sequence list
+        gt_res:  decode后的ground truth list
         '''
-
-        assert isinstance(model, OfaForAllTasks), \
-            "model required to be OfaForAllTasks type, got {}".format(type(model))
-        
         model.model.eval()
         with torch.no_grad():
             # generate the candidates with beam decoding
             gen_candidates=model.generator.generate([model.model],
                                               inputs,
-                                              prefix_tokens=input.get(
+                                              prefix_tokens=inputs.get(
                                                   'prefix_tokens', None))
         gen_target=[]
         gen_res=[]
         gt_res=[]
-        for i in len(gen_candidates):
+        # beam_size=5
+        for i in range(len(gen_candidates)):
             # randomly pick the candidate
-            # 虽然很确信i一定只能是0, 但是还是先这么写着吧
-            lucky_boy=random.choice(gen_candidates[i])
+            # len(gen_candidates)==batch_size
+            lucky_boy_id=random.randint(0, len(gen_candidates[i])-1)
+            lucky_boy=gen_candidates[i][lucky_boy_id]["tokens"]
             # save token
             gen_target.append(lucky_boy.int())
             # save decoded gen and ground truth
-            gen_res.append(model.tokenizer.batch_decode(lucky_boy))
+            gen_res.append(self.tokenizer.decode(lucky_boy).strip())
+            # remove padding index
+            ground_truth=inputs["target"][i]
+            ground_truth=ground_truth[ground_truth.ne(self.padding_idx)]
 
-            gt_res.append(model.tokenizer.batch_decode(inputs["target"]))
+            gt_res.append(self.tokenizer.decode(ground_truth))
+
+        # gen_target: List[Tensor]
+        # gen_res: List[str]
+        # gt_res: List[str]
         return gen_target, gen_res, gt_res
