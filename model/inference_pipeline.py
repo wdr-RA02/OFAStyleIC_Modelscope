80e0875ac0ed8288944b33e18ecc675a9af85286
wdr-RA02 <cydia1829@gmail.com>
2023-05-07 11:10:29 +0000

    fix inference bug; mod .gitignore

--- model/inference_pipeline.py
+++ model/inference_pipeline.py
@@ -77,31 +77,6 @@ def start_inference_from_eval(train_conf: dict,
     stylish_ic=generate_infr_pipeline(train_conf, mod_fn)
 
 
-def start_inference_from_json(train_conf: Dict[str,str],
-                              json_file: str, 
-                              mod_fn: Callable):
-    # define preprocessor and model
-    stylish_ic=generate_infr_pipeline(train_conf, mod_fn)
-
-    with open(json_file, "r") as f:
-        # needs to follow format of {[{"image","style",("reference")}]}
-        data=json.load(f)
-        if "image_hash" in data:
-            data=get_eval_batch(data)
-        elif "image" not in data:
-            raise KeyError("json file missing key 'image'")
-
-    result=[]
-    result_cap=list(map(lambda x:x.get(OutputKeys.CAPTION), stylish_ic(data)))
-    for i in range(len(result_cap)):
-        # add original text and style to captions
-        result.append({
-            **data[i],
-            "caption": result_cap[i][0]
-        })
-    return result
-
-
 def start_inference_from_eval(train_conf: dict,
                    data: List[Dict[str,str]],
                    mod_fn: Callable):
